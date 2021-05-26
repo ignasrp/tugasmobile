51470b9ecd1448a18b943ee6a5924ddb05d17297
ignasrp <ignathius.patiung@yahoo.com>
2021-05-26 20:28:28 +0800

    tugas mobile

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
-# Belajar Room DB (MVVM, Coroutines, LiveData)
-Repo ini untuk tutorial dari artikel medium
-
-https://medium.com/@andiwijayaid/belajar-room-db-mvvm-coroutines-livedata-7fe4ebf771de
-
-![alt text](https://github.com/andiwijayaid/room-mvvm-coroutines-livedata/blob/master/images/note.gif)
+Ignathius Rahardianto Patiung
+D121181334
---
+++ app/src/main/java/com/example/Room/ui/main/NoteViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.Room.ui.main
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.example.Room.db.note.Note
+import com.example.Room.db.note.NoteRepository
+
+class NoteViewModel(application: Application) : AndroidViewModel(application) {
+
+    private var noteRepository = NoteRepository(application)
+    private var notes: LiveData<List<Note>>? = noteRepository.getNotes()
+
+    fun insertNote(note: Note) {
+        noteRepository.insert(note)
+    }
+
+    fun getNotes(): LiveData<List<Note>>? {
+        return notes
+    }
+
+    fun deleteNote(note: Note) {
+        noteRepository.delete(note)
+    }
+
+    fun updateNote(note: Note) {
+        noteRepository.update(note)
+    }
+
+}
